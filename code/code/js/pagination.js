1c04fed468f2179e14a0ca4cebd6d46bc446c90f
cyprien <cyprien.tavel@gmail.com>
2022-10-29 15:22:28 +0200

    responsive

--- code/code/js/pagination.js
+++
@@ -1,9 +0,0 @@
-function change(numero){
-	document.getElementById('lien' + numero).classList.add('actif');
-
-	for(let i = 1; i < 5; i++){
-		if (i != numero){
-			document.getElementById('lien' + i).classList.remove('actif');
-		}
-	}
-}
