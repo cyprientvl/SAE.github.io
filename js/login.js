86e668d94f3e30617fce9e1e104be61969e65ab7
cyprien <109107357+cyprientvl@users.noreply.github.com>
2022-11-23 21:11:01 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 function openconnecter(){
-	document.querySelector("div.connextion").classList.add("connextion-click");
-	document.querySelector("div.responsive").classList.add("display-none");
+	document.querySelector(".connextion").classList.add("connextion-click");
+	document.querySelector(".responsive").classList.add("display-none");
 }
 
 function openregister(){
-	document.querySelector("div.register").classList.add("connextion-click");
-	document.querySelector("div.responsive").classList.add("display-none");
+	document.querySelector(".register").classList.add("connextion-click");
+	document.querySelector(".responsive").classList.add("display-none");
 }
 
 function back() {
 
-	document.querySelector("div.connextion").classList.remove("connextion-click");
-	document.querySelector("div.register").classList.remove("connextion-click");
-	document.querySelector("div.responsive").classList.remove("display-none");
+	document.querySelector(".connextion").classList.remove("connextion-click");
+	document.querySelector(".register").classList.remove("connextion-click");
+	document.querySelector(".responsive").classList.remove("display-none");
 
 }
